050cee43583ad5ed7f8690b31c0509529bce0eeb
fernandoflores1 <115038723+fernandoflores1@users.noreply.github.com>
2024-09-25 17:14:36 +0200

    Contador fecha inicial

--- src/js/script.js
+++ src/js/script.js
@@ -1,43 +1,42 @@
 let bodyElement = document.getElementById("main");
-let fechaObjetivo = document.getElementById("fechaObjetivo");
-let fechaEscogida = new Date("10/20/2024");
 
-let segundoObjetivo = fechaEscogida.getSeconds;
-let minutoObjetivo = fechaEscogida.getMinutes;
-let horaObjetivo = fechaEscogida.getHours;
-let diaObjetivo = fechaEscogida.getDay;
-let mesObjetivo = fechaEscogida.getMonth;
-let anioObjetivo = fechaEscogida.getFullYear;
+let fechaEscogida = new Date(2024, 11,15);
+fechaEscogida.setHours(0, 0, 0, 0);
+
+let countdown = document.createElement("p");
+
+let inputElement = document.createElement("input");
+inputElement.setAttribute("type", "date");
+inputElement.setAttribute("name", "fecha");
+inputElement.setAttribute("id", "fechaObjetivo");
 
 
-
+bodyElement.append(countdown);
+bodyElement.append(inputElement);
 
 let nombreIntervalo = setInterval(() => {
-    
-    let fechaActual = new Date();
-    let segundo = fechaActual.getSeconds;
-    let minuto = fechaActual.getMinutes;
-    let hora = fechaActual.getHours;
-    let dia = fechaActual.getDay;
-    let mes = fechaActual.getMonth;
-    let anio = fechaActual.getFullYear;
+    const now = new Date();
 
-    
+    if (fechaEscogida > now) {
+        const totalMilliseconds = fechaEscogida.getTime() - now.getTime();
+        const totalSeconds = Math.trunc(totalMilliseconds / 1000);
+        const seconds = totalSeconds % 60;
+        const totalMinutes = Math.trunc(totalSeconds / 60);
+        const minutes = totalMinutes % 60;
+        const totalHours = Math.trunc(totalMinutes / 60);
+        const hours = totalHours % 24;
+        const totalDays = Math.trunc(totalHours / 24);
 
+        let months = (fechaEscogida.getFullYear() - now.getFullYear()) * 12 + (fechaEscogida.getMonth() - now.getMonth());
+        const days = totalDays % 30;
 
+        countdown.textContent = `Mes(es): ${months}, Día(s): ${days}, Hora(s): ${hours}, Minuto(s): ${minutes}, Segundo(s): ${seconds}`;
 
+    }else{
+        clearInterval(nombreIntervalo);
+        countdown.textContent = "¡¡HAPPY BIRTHDAY!!"
+    }
     
 }, 1000);
-let segundosRestantes = segundo - segundoObjetivo;
-let minutosRestantes = minuto - minutoObjetivo;
-let horasRestantes = hora - horaObjetivo;
-let diasRestantes = dia - diaObjetivo;
-let mesesRestantes = mes - mesObjetivo;
-let aniosRestantes = anio - anioObjetivo;
-
-
-if(segundosRestantes&&minutosRestantes&&horasRestantes&&diasRestantes&&mesesRestantes&&aniosRestantes == 0){
-    clearInterval(nombreIntervalo);
-}
 
 
