94a2dfef465327ac5ae6aa734655b2ad682fe04a
fernandoflores1 <115038723+fernandoflores1@users.noreply.github.com>
2024-10-07 17:11:19 +0200

    New countdown without errors

--- src/js/script.js
+++ src/js/script.js
@@ -16,11 +16,18 @@ inputElement.setAttribute("id", "objetiveDate");
 
 let newDate = null;
 let targetTime = null;
+let countdownInterval = null;
 
 inputElement.addEventListener("change", () => {
     newDate = new Date(inputElement.value);
     newDate.setHours(0, 0, 0, 0);
     targetTime = newDate.getTime();
+
+    if (countdownInterval) {
+        clearInterval(countdownInterval);
+    }
+    startCountdown();
+
 });
 
 bodyElement.append(divElement);
@@ -29,32 +36,33 @@ divCountown.append(h1Element);
 divCountown.append(countdown);
 divCountown.append(inputElement);
 
+function startCountdown() {
+    countdownInterval = setInterval(() => {
+        const now = new Date().getTime();
 
-let countdownIntervale = setInterval(() => {
-    const now = new Date().getTime();
+        if (targetTime) {
+            let remainingTime = targetTime - now;
+            if (remainingTime > 0) {
+                const totalMonths = Math.trunc(remainingTime / (1000 * 60 * 60 * 24 * 30));
+                const remainingDays = Math.trunc((remainingTime % (1000 * 60 * 60 * 24 * 30)) / (1000 * 60 * 60 * 24));
+                const hours = Math.trunc((remainingTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+                const minutes = Math.trunc((remainingTime % (1000 * 60 * 60)) / (1000 * 60));
+                const seconds = Math.trunc((remainingTime % (1000 * 60)) / 1000);
 
-    if (targetTime) {
-        let remainingTime = targetTime - now;
-        if (remainingTime > 0) {
-            const totalMonths = Math.trunc(remainingTime / (1000 * 60 * 60 * 24 * 30));
-            const remainingDays = Math.trunc((remainingTime % (1000 * 60 * 60 * 24 * 30)) / (1000 * 60 * 60 * 24));
-            const hours = Math.trunc((remainingTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-            const minutes = Math.trunc((remainingTime % (1000 * 60 * 60)) / (1000 * 60));
-            const seconds = Math.trunc((remainingTime % (1000 * 60)) / 1000);
+                if (totalMonths >= 1) {
+                    countdown.setAttribute("class", "green");
+                } else if (totalMonths == 0 && remainingDays >= 7) {
+                    countdown.setAttribute("class", "orange");
+                } else if (remainingDays < 7) {
+                    countdown.setAttribute("class", "red");
+                }
 
-            if (totalMonths >= 1) {
-                countdown.setAttribute("class", "green");
-            } else if (totalMonths == 0 && remainingDays >= 7) {
-                countdown.setAttribute("class", "orange");
-            } else if (remainingDays < 7) {
-                countdown.setAttribute("class", "red");
+                countdown.textContent = `${totalMonths} months, ${remainingDays} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds`;
+            } else if (remainingTime < 0) {
+                clearInterval(countdownInterval);
+                countdown.textContent = "HAPPY BIRTHDAY !!";
+                countdown.setAttribute("class", "felicidades");
             }
-
-            countdown.textContent = `${totalMonths} months, ${remainingDays} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds`;
-        } else if (remainingTime < 0) {
-            clearInterval(countdownIntervale);
-            countdown.textContent = "HAPPY BIRTHDAY !!";
-            countdown.setAttribute("class", "felicidades");
         }
-    }
-}, 1000);
+    }, 1000);
+}
